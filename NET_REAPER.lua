decd99eb43ae9369ef072ebb456bce91bea07724
SikkeDotExe <81999512+SikkeDotExe@users.noreply.github.com>
2024-02-09 09:33:57 -0500

    Update NET_REAPER.lua
    Updated github link.

--- NET_REAPER.lua
+++ NET_REAPER.lua
@@ -25,11 +25,12 @@ end
 if auto_updater == true then error("Invalid auto-updater lib. Please delete your Stand/Lua Scripts/lib/auto-updater.lua and try again") end
 
 auto_updater.run_auto_update({
-    source_url="https://raw.githubusercontent.com/MyUsername/MyProjectName/main/MyScriptName.lua",
+    source_url="https://raw.githubusercontent.com/SikkeDotExe/gwapdaddy/main/NET_REAPER.lua",
     script_relpath=SCRIPT_RELPATH,
     verify_file_begins_with="--"
 })
 
+-- Script
 local PLAYERS_LIST
 local PLAYERS_COUNT
 local IS_CLOSING = false
